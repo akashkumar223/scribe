86a0a5d865d8c0e212fe10a09de890004a4f0224
Akash kumar <128918989+akashkumar223@users.noreply.github.com>
2026-09-22 16:07:44 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,10 @@ import tempfile
 from dotenv import load_dotenv
 load_dotenv()
 
+port = int(os.environ.get("PORT", 8000))
+uvicorn.run("main:app", host="0.0.0.0", port=port)
+
+
 from fastapi import FastAPI, UploadFile, File, HTTPException, Response
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
